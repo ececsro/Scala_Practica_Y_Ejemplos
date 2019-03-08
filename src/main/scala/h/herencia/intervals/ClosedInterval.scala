afabe9d4a9bdfd7494117cd020edf4dbeaec45e7
ECECSRO <casaraog@gmail.com>
2019-03-08 16:08:13 +0100

    Solo Practica Scala

--- src/main/scala/h/herencia/intervals/ClosedInterval.scala
+++
@@ -1,9 +0,0 @@
-package h.herencia.intervals
-
-class ClosedInterval(__min:Double, __max:Double) extends Interval(__min, __max){
-
-  override def includes(value: Double): Boolean =
-    this.min <= value && value <= this.max
-
-  override def toString = "[" + super.toString + "]"
-}
